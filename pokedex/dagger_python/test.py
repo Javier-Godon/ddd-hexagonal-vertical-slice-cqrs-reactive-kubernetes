7d94b38495caf9885d4a24074ac8579b97876919
Javier Godon <javier.godon@gmail.com>
2024-12-16 12:23:21 +0100

    chore: added some files for testing in dagger with kcl_lib

---
+++ pokedex/dagger_python/test.py
@@ -0,0 +1,11 @@
+import kcl_lib.api as api
+
+args = api.ExecProgram_Args(k_filename_list=["schema.k"])
+api = api.API()
+yaml_result=api.exec_program(args).yaml_result
+with open("new.yaml", 'w') as f:
+    f.write(yaml_result)
+
+
+
+
