fb9130b7eaf23a14ca66e445c5b161fba9376cc9
Javier Godon <javier.godon@gmail.com>
2024-12-16 20:03:53 +0100

    test: wip (changes for testing CI/CD)

--- pokedex/dagger_python/test.py
+++ pokedex/dagger_python/test.py
@@ -6,17 +6,6 @@ yaml_result=api.exec_program(args).yaml_result
 with open("new.yaml", 'w') as f:
     f.write(yaml_result)
 
-# input_file = 'apps/deployments/pokedex/dev'
-# output_file = 'apps/deployments/pokedex/dev/manifests/kubernetes-manifests.yaml'
-#
-# args = api.ExecProgram_Args(k_filename_list=[input_file])
-# api = api.API()
-# yaml_result = api.exec_program(args).yaml_result
-#
-# with open(output_file, 'w') as f:
-#     f.write(yaml_result)
-#
-# print(f'YAML manifests generated and saved to {output_file}')
 
 
 
